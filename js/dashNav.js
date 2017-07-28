8e849cc8fa702b87e41292db6ef277a17d91ace0
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-07-27 22:11:55 -0400

    ADDING ALL THE RESOURCES FOR THE PROJECT

---
+++ js/dashNav.js
@@ -0,0 +1,22 @@
+function home(){
+    window.location.href = "index.html";
+}
+function samples(){
+    window.location.href = "codeSamples.html";
+}
+function contactMe(){
+    window.location.href = "contactMe.html";
+}
+function help(){
+    window.location.href = "help.html";
+}
+function downloadResume(){
+    document.location ="downloads/Resume.pdf"
+}
+function linkedIn(){
+    window.location.href = "https://www.linkedin.com/in/mario-merendino-561138127/";
+}
+function changePicture(){
+    var image = document.getElementById("portrait").src;
+    alert(image);
+}
