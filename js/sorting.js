309913351ad64eae933979bd84a65e04382d76d0
Mario Merendino <mmerendino@mdsol.com>
2017-08-04 17:18:30 -0400

    Started Progress

--- js/sorting.js
+++ js/sorting.js
@@ -1,28 +1,47 @@
 var arr = []
+var startButton = document.getElementById("startButton")
+startButton.onclick = function run(){
+    var length = document.getElementById("size").value;
+    var term = document.getElementById("output");
+    term.innerHTML = "";
+    //Fills Array
+    fillArray(length);
+    
+    //output array;
+    var listElement = document.createElement("li");
+    listElement.innerHTML = arrayAsString(length);
+    term.appendChild(listElement);
 
-function main(){
     
 }
-
+function arrayAsString(size){
+    var str = arr[0].toString();
+    for(var i = 1; i < size; i++){
+        str = str + ", " + arr[i].toString(); 
+    }
+    alert(str)
+    return str
+}
 //Fills Array with Random variables.
 function fillArray(size){
     for(var i = 0; i < size; i++){
-        arr[i] = Math.random();
+        arr[i] = Math.floor(Math.random() * 100);
     }
 }
 
 //Bubble Sort
-function bubbleSort(){
+function bubbleSort(a,size){
     var temp
-    for (var n=0; n <arr.length; n++){
-        for (var i=0; i <arr.legnth; i++){
+    for (var n=0; n <size; n++){
+        for (var i=0; i <size; i++){
             if(a[i]> a[i+1]){
                 temp=a[i];
-                arr[i]=a[i+1];
-                arr[i+1]=temp;
+                a[i]=a[i+1];
+                a[i+1]=temp;
             }
         }
     }
+    return a
 }
 //Insertion Sort
 function insertionSort(){
