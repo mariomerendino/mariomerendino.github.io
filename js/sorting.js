47de0294e8565766e65fdb8db2b61592b8e5ce1f
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-08-04 23:05:07 -0400

    Added More algs

--- js/sorting.js
+++ js/sorting.js
@@ -1,5 +1,6 @@
 var arr = []
 var startButton = document.getElementById("startButton")
+
 startButton.onclick = function run(){
     var length = document.getElementById("size").value;
     var term = document.getElementById("output");
@@ -9,17 +10,48 @@ startButton.onclick = function run(){
     
     //output array;
     var listElement = document.createElement("li");
-    listElement.innerHTML = arrayAsString(length);
+    listElement.innerHTML = "Original Array: "+ arrayAsString(arr, length);
     term.appendChild(listElement);
+    console.log(arr)
 
-    
+    //output performance of Bubbble Sort
+    var t0 = performance.now();
+    var bubbleSorted = bubbleSort(copyArray(length), length);
+    var t1 = performance.now();
+    var bubbleSortLine = document.createElement("li");
+    bubbleSortLine.innerHTML = "BubbleSort took " + (t1 - t0) + " milliseconds.";
+    term.appendChild(bubbleSortLine);
+
+    //output performance of Insertion sort
+    t0 = performance.now();
+    var insertionSorted = insertionSort(copyArray(length), length);
+    t1 = performance.now();
+    var insertionSortLine = document.createElement("li");
+    insertionSortLine.innerHTML = "Insertion Sort took " + (t1 - t0) + " milliseconds.";
+    term.appendChild(insertionSortLine);
+
+    //output performance of Merge Sort
+    t0 = performance.now();
+    var mergeSorted = mergeSort(copyArray(length), length);
+    console.log(mergeSorted)
+    t1 = performance.now();
+    var mergeSortLine = document.createElement("li");
+    mergeSortLine.innerHTML = "Merge Sort took " + (t1 - t0) + " milliseconds.";
+    term.appendChild(mergeSortLine);
+
 }
-function arrayAsString(size){
-    var str = arr[0].toString();
-    for(var i = 1; i < size; i++){
-        str = str + ", " + arr[i].toString(); 
+function copyArray(size){
+    var a = [];
+    for(var i = 0; i < size; i++){
+        a[i] = arr[i]
     }
-    alert(str)
+    return a;
+}
+function arrayAsString(a, size){
+    var str = a[0].toString();
+    for(var i = 1; i < size; i++){
+        str = str + ", " + a[i].toString(); 
+    }
     return str
 }
 //Fills Array with Random variables.
@@ -44,21 +76,53 @@ function bubbleSort(a,size){
     return a
 }
 //Insertion Sort
-function insertionSort(){
+function insertionSort(a, size){
     var key;
     // Start with 1 (The second number in the array.
-    for(var j = 1; j < arr.length; j++){
+    for(var j = 0; j < size; j++){
         //set that equal to the key   
-        key = arr[j]; 
+        key = a[j]; 
         // larger values move up in the array
         for(var i = j - 1; i >= 0; i--){
-            if(arr[i] > key)
-                arr[i+1] = a[i];
+            if(a[i] > key)
+                a[i+1] = a[i];
         }
         //Put key into its proper location
-        arr[i+1] = key;    
+        a[i+1] = key;    
     }
-     return;
+    console.log(a)
+    return a;
 }
 //Merge Sort function
-//
+function mergeSort(a,length)
+{
+    if (length < 2)
+        return a;
+ 
+    var middle = parseInt(length / 2);
+    var left   = a.slice(0, middle);
+    var right  = a.slice(middle, length);
+ 
+    return merge(mergeSort(left), mergeSort(right));
+}
+ 
+function merge(left, right)
+{
+    var result = [];
+ 
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+ 
+    while (left.length)
+        result.push(left.shift());
+ 
+    while (right.length)
+        result.push(right.shift());
+ 
+    return result;
+}
